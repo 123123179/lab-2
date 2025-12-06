285c6b3633bec4fae26cc0ae0a6c208dc72ecc67
123123179 <241265725+123123179@users.noreply.github.com>
2025-12-06 15:17:58 +0200

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/currency', methods=['GET'])
 def currency():
-    # отримуємо параметри з URL
+    # отримуємо параметри з URL: http://127.0.0.1:8000/currency?today&key=value
     today = request.args.get('today')
     key = request.args.get('key')
 
@@ -14,3 +14,4 @@ def currency():
 
 if __name__ == '__main__':
     app.run(port=8000)
+
